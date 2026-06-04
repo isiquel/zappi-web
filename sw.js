1e1c0d02012423fb52a737dafad0cc9697dd199d
isiquel <isiquelcamilanatan@gmail.com>
2026-06-04 17:50:48 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,30 @@
-const CACHE_NAME = "zappi-web-v1";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
+const CACHE_NAME = "zappi-web-v1-6";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
+
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request).catch(() => caches.match("/"))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(request).catch(() => caches.match(request))
   );
 });
